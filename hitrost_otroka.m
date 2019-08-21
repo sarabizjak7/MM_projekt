c93e1432c91ba6eb315727c71051823bad2a626c
Sara <sarabizjak97@gmail.com>
2019-08-21 17:32:26 +0200

    Delete hitrost_otroka.m

--- hitrost_otroka.m
+++
@@ -1,1 +0,0 @@
-function [velikost_hitrosti, smer] = hitrost_otroka(dx, dy, t)    % HITROST_OTROKA izracuna velikost vektorja hitrosti in smer otroka ob času t        dx_ = dx(t);    dy_ = dy(t);        velikost_hitrosti = sqrt(dx_.^2 + dy_.^2);    smer = [dx_; dy_];end

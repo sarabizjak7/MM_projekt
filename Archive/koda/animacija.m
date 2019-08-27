53543b22fff4559b7b78ae6df0d16f305bf3bc2d
sarabizjak7 <sarabizjak97@gmail.com>
2019-08-27 15:53:28 +0200

    Popravek v poročilu

---
+++ Archive/koda/animacija.m
@@ -0,0 +1,26 @@
+function animacija(x_otrok, y_otrok, x_igraca, y_igraca, t_igrace)
+% ANIMACIJA izrise animacijo gibanja otroka in igrace po krivuljah		
+%		
+%   VHODNI PODATKI:		
+%       x_otrok, y_otrok: koordinate otroka ob casih t		
+%       x_igraca, y_igraca: koordinate igrace ob casih t
+
+    for j = 1 : size(t_igrace)
+        
+        x_o = x_otrok(t_igrace(j));
+        y_o = y_otrok(t_igrace(j));
+        
+        x_i = x_igraca(j);
+        y_i = y_igraca(j);
+
+        tocka_o = plot(x_o, y_o, 'ob', 'MarkerFaceColor','b', 'MarkerSize', 7);
+        tocka_i = plot(x_i, y_i, 'or', 'MarkerFaceColor','r', 'MarkerSize', 7);
+
+        pause(0.1);
+
+        delete(tocka_o);
+        delete(tocka_i);
+
+    end
+
+   
